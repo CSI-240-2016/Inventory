0a788b7fde39723768ab7b732444876992c18db3
temportalflux <dustin.yost.t@gmail.com>
2016-04-18 18:28:40 +0000

    Add member variables to Item class

--- src/Item.h
+++ src/Item.h
@@ -2,12 +2,18 @@
 #ifndef ITEM_H
 #define ITEM_H
 
+#include "Source.h"
+
 using namespace std;
 
 class Item {
 
 	private:
-		
+		int mSerialNumber;
+		String mName, mType, mOwnerName;
+		Source mSource;
+		Location mLocation;
+		bool mIsIn;
 	
 	protected:
 		
