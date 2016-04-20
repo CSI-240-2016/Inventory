9a735fec6c89b1f5fb15efec8c6bc551bd3d1d45
DerikGagnon <derik.gagnon@mymail.champlain.edu>
2016-04-20 18:26:19 -0400

    Finished file for general frontend functions

--- src/common.cpp
+++ src/common.cpp
@@ -15,17 +15,35 @@ void clearScreen()
 
 void displayAdminMenu()
 {
-
+	cout << setw(29) << right << "Administrator Menu:\n\n\n";
+	cout << setw(12) << right << "1.) " << left << "Check In and Check Out\n";
+	cout << setw(12) << right << "2.) " << left << "Items\n";
+	cout << setw(12) << right << "3.) " << left << "Clubs\n";
+	cout << setw(12) << right << "4.) " << left << "Users\n";
+	cout << setw(12) << right << "5.) " << left << "Search\n";
+	cout << setw(12) << right << "6.) " << left << "Export\n";
+	cout << setw(12) << right << "7.) " << left << "Log Out\n";
 }
 
 void displayGeneralMenu()
 {
-
+	cout << setw(22) << right << "Public Menu:\n\n\n";
+	cout << setw(12) << right << "1.) " << left << "Items\n";
+	cout << setw(12) << right << "2.) " << left << "Search\n";
+	cout << setw(12) << right << "3.) " << left << "Log In as Admin\n";
+	cout << setw(12) << right << "4.) " << left << "Shutdown\n";
 }
 
 void displayLogo()
 {
-
+	clearScreen();
+	cout << endl << endl <<
+		"    _____  _____       " << endl <<
+		"   / ____|/ ____|   /\\" << endl <<
+		"  | (___ | |  __   /  \\" << endl <<
+		"   \\___ \\| | |_ | / /\\ \\" << endl <<
+		"   ____) | |__| |/ ____ \\" << endl <<
+		"  |_____/ \\_____/_/    \\_\\" << endl << endl << endl;
 }
 
 void pause()
