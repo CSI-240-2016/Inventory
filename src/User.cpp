d2345fcb752417928a0903d62035844ee2af78a1
temportalflux <dustin.yost.t@gmail.com>
2016-04-23 00:59:39 +0000

    Patch compile time errors of String and Person

--- src/User.cpp
+++ src/User.cpp
@@ -36,3 +36,40 @@ void	User::setUserName(string username) {
 void	User::setPassword(string password) {
 	mPassword = password;
 }
+
+// ~~~~~~~~~~ Operators ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+/** Purpose: OVERLOAD Loads variables from a file stream
+ * PRE:  the file stream, the class instance
+ * POST: loads variables from stream, returns the stream
+ */
+ifstream& operator>>(ifstream& stream, User& obj) {
+	getline(stream, obj.mID);
+	obj.setIDValue();
+	getline(stream, obj.mName);
+	getline(stream, obj.mAddress);
+	getline(stream, obj.mPhoneNumber);
+	return stream;
+}
+
+/** Purpose: OVERLOAD Loads variables from a console stream
+ * PRE:  the stream, the class instance
+ * POST: outputs prompts, loads variables from stream, returns the stream
+ */
+istream& operator>>(istream& stream, User& obj) {
+	
+	cout << "Enter name: ";
+	getline(stream, obj.mName);
+	
+	cout << "Enter ID: ";
+	getline(stream, obj.mID);
+	obj.setIDValue();
+	
+	cout << "Enter address: ";
+	getline(stream, obj.mAddress);
+	
+	cout << "Enter phone number: ";
+	getline(stream, obj.mPhoneNumber);
+	
+	return stream;
+}
