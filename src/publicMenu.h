2f7e38f459edf62d7af5b9abb2841705cd103295
DerikGagnon <derik.gagnon@mymail.champlain.edu>
2016-04-25 13:13:38 -0400

    Merging

--- src/publicMenu.h
+++ src/publicMenu.h
@@ -4,11 +4,7 @@
 #include "common.h"
 #include <iomanip>
 #include <string>
-<<<<<<< HEAD
-#include <conio.h>
-=======
 // doesnt compile on unix #include <conio.h>
->>>>>>> 2718255af37e8e8624dae9e445cbf2fd1dfe0da5
 #include "LinkedList.h"
 #include "Item.h"
 
